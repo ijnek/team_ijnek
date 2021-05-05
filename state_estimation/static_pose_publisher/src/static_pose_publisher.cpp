aafc5170f094dd27385502017c22173a8bb402bd
Kenji Brameld <kenjibrameld@gmail.com>
2021-05-05 17:55:14 +0900

    fix up usage of initial_pose_theta argument

--- state_estimation/static_pose_publisher/src/static_pose_publisher.cpp
+++ state_estimation/static_pose_publisher/src/static_pose_publisher.cpp
@@ -1,8 +1,8 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/msg/transform_stamped.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/msg/transform_stamped.h>
 
 using namespace std::chrono_literals;
 
@@ -26,17 +26,20 @@ private:
 
     void timer_callback()
     {
+        tf2::Quaternion quaternion;
+        quaternion.setRPY(0, 0, get_parameter("initial_pose_theta").as_double());
+
         geometry_msgs::msg::TransformStamped transformStamped;
         transformStamped.header.stamp = now();
         transformStamped.header.frame_id = "map";
         transformStamped.child_frame_id = "base_link";
         transformStamped.transform.translation.x = get_parameter("initial_pose_x").as_double();
         transformStamped.transform.translation.y = get_parameter("initial_pose_y").as_double();
-        transformStamped.transform.translation.z = get_parameter("initial_pose_theta").as_double();
-        transformStamped.transform.rotation.x = 0.0;
-        transformStamped.transform.rotation.y = 0.0;
-        transformStamped.transform.rotation.z = 0.0;
-        transformStamped.transform.rotation.w = 1.0;
+        transformStamped.transform.translation.z = 0.0;
+        transformStamped.transform.rotation.x = quaternion.getX();
+        transformStamped.transform.rotation.y = quaternion.getY();
+        transformStamped.transform.rotation.z = quaternion.getZ();
+        transformStamped.transform.rotation.w = quaternion.getW();
         tf_broadcaster_->sendTransform(transformStamped);
     }
 };
