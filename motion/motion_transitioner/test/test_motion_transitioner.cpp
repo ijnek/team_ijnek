9abb957e8d026de2b7b988f0d7f97ab697e3f072
Kenji Brameld <kenjibrameld@gmail.com>
2021-06-06 19:51:48 +0900

    rename test_motion_transitioner_2 to motion_transitioner

---
+++ motion/motion_transitioner/test/test_motion_transitioner.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include "motion_transitioner/motion_transitioner.hpp"
+
+bool startGetupCalled = false;
+bool startKickCalled = false;
+bool startCrouchCalled = false;
+
+void startGetup(Getup)
+{
+  startGetupCalled = true;
+}
+
+void startKick(Kick)
+{
+  startKickCalled = true;
+}
+
+void startCrouch(Crouch)
+{
+  startCrouchCalled = true;
+}
+
+TEST(TestMotionTransitioner, Test1)
+{
+  MotionTransitioner motionTransitioner(startGetup, startKick, startCrouch);
+  motionTransitioner.request(Getup{});
+  ASSERT_TRUE(startGetupCalled);
+
+  motionTransitioner.notifyGetupDone();
+  ASSERT_TRUE(startCrouchCalled);
+}
+
+TEST(TestMotionTransitioner, Test2)
+{
+  MotionTransitioner motionTransitioner(startGetup, startKick, startCrouch);
+  motionTransitioner.request(Getup{});
+  motionTransitioner.request(Kick{});
+  ASSERT_FALSE(startKickCalled);
+
+  motionTransitioner.notifyGetupDone();
+  motionTransitioner.request(Kick{});
+  ASSERT_TRUE(startKickCalled);
+}
