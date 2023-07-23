a1d79f502e7ef01d395de7aac4c8d8e03abed722
ijnek <kenjibrameld@gmail.com>
2023-07-23 16:09:45 +0000

    lower arms, and run nao_interfaces_bridge_node
    Signed-off-by: ijnek <kenjibrameld@gmail.com>

--- motion/motion_launch/launch/motion_webots.launch.py
+++ motion/motion_launch/launch/motion_webots.launch.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, ExecuteProcess, LogInfo, RegisterEventHandler,
-                            TimerAction)
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription,
+                            LogInfo, RegisterEventHandler, TimerAction)
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -38,7 +39,30 @@ def generate_launch_description():
     ik_node = Node(package='nao_ik', executable='ik_node')
     nao_phase_provider_node = Node(package='nao_phase_provider', executable='nao_phase_provider',
                                    remappings=[('fsr', '/sensors/fsr')])
-    walk_node = Node(package='walk', executable='walk')
+    walk_node = Node(package='walk', executable='walk', remappings=[('imu', '/sensors/imu')])
+
+    nao_state_publisher_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare('nao_state_publisher'),
+                    'launch',
+                    'nao_state_publisher_launch.py'])))
+
+    nao_interfaces_bridge_node = Node(
+        package='nao_interfaces_bridge', executable='nao_interfaces_bridge')
+
+    lower_arms = ExecuteProcess(
+        cmd=['ros2 topic pub --once /effectors/joint_positions nao_command_msgs/msg/JointPositions "{indexes: [2, 18], positions: [1.517, 1.517]}"'],
+        shell=True)
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('motion_launch'), 'rviz', 'motion_webots.rviz'])
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+    )
 
     return LaunchDescription([
         perspective_arg,
@@ -59,4 +83,8 @@ def generate_launch_description():
         ik_node,
         nao_phase_provider_node,
         walk_node,
+        nao_state_publisher_launch,
+        nao_interfaces_bridge_node,
+        lower_arms,
+        rviz_node,
     ])
