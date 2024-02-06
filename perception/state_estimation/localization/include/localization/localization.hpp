63b69a5d584fb49c473bafe333d15664b1fe11f7
Kenji Brameld <kenjibrameld@gmail.com>
2024-02-06 03:55:40 +0000

    added localization node as rclcpp component
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

---
+++ perception/state_estimation/localization/include/localization/localization.hpp
@@ -0,0 +1,31 @@
+#ifndef LOCALIZATION__LOCALIZATION_HPP_
+#define LOCALIZATION__LOCALIZATION_HPP_
+
+#include <memory>
+
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "soccer_vision_3d_msgs/msg/marking_array.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+
+namespace localization
+{
+
+class Localization : public rclcpp::Node
+{
+public:
+  explicit Localization(const rclcpp::NodeOptions & options = rclcpp::NodeOptions{});
+
+private:
+
+  void markings_callback(const soccer_vision_3d_msgs::msg::MarkingArray::SharedPtr msg);
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+  rclcpp::Subscription<soccer_vision_3d_msgs::msg::MarkingArray>::SharedPtr sub_markings_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+};
+
+}  // namespace localization
+
+#endif  // LOCALIZATION__LOCALIZATION_HPP_
