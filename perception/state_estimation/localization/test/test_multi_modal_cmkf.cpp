2ef680d49b06911bce90f3295112d19a96242b9b
Kenji Brameld <kenjibrameld@gmail.com>
2024-04-29 03:17:03 +0000

    add test for update method
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- perception/state_estimation/localization/test/test_multi_modal_cmkf.cpp
+++ perception/state_estimation/localization/test/test_multi_modal_cmkf.cpp
@@ -33,3 +33,30 @@ TEST(TestMultiModalCMKF, TestMoveOneMeterForward)
   EXPECT_NEAR(pose_with_covariance.pose.position.y, 0.0, 0.01);
   EXPECT_NEAR(pose_with_covariance.pose.position.z, 0.0, 0.01);
 }
+
+TEST(TestMultiModalCMKF, TestUpdateFarRightCorner)
+{
+  localization::MultiModalCMKF multi_modal_cmkf;
+
+  // Let robot be at (3.5, -3.0, 0.0), looking at the corner marker at (4.5, -3.0)
+
+  // Corner marker 1.0m in front of the robot, with two rays:
+  // - First ray facing in -x
+  // - Second ray facing in +y
+  soccer_vision_3d_msgs::msg::MarkingArray marking_array;
+  marking_array.header.frame_id = "base_footprint";
+  soccer_vision_3d_msgs::msg::MarkingIntersection marking_intersection;
+  marking_intersection.center.x = 1.0;
+  marking_intersection.num_rays = 2;
+  marking_intersection.rays = {
+    geometry_msgs::msg::Vector3().set__x(-1.0).set__y(0.0).set__z(0.0),
+    geometry_msgs::msg::Vector3().set__x(0.0).set__y(1.0).set__z(0.0)};
+  marking_array.intersections.push_back(marking_intersection);
+
+  multi_modal_cmkf.update(marking_array);
+
+  // Pose should be updated to (3.5, -3.0, 0.0), if initial covariance is high enough
+  auto pose_with_covariance = multi_modal_cmkf.getPoseWithCovariance();
+  EXPECT_NEAR(pose_with_covariance.pose.position.x, 3.5, 0.01);
+  EXPECT_NEAR(pose_with_covariance.pose.position.y, -3.0, 0.01);
+}
